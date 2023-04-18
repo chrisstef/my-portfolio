49e9d6e22f782e53fcb20e4924fba8cf4eb033e7
Christos Stefanis <xristostefanis@gmail.com>
2023-04-19 01:42:48 +0200

    Update Skills, Work & remove unecessary assets

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -1,69 +1,17 @@
 import email from "../assets/email.png";
-import git from "../assets/git.png";
-import javascript from "../assets/javascript.png";
 import solidity from "../assets/solidity.png";
-import node from "../assets/node.png";
 import python from "../assets/python.png";
 import react from "../assets/react.png";
-import redux from "../assets/redux.png";
-import typescript from "../assets/typescript.png";
-import whatsapp from "../assets/whatsapp.png";
-import ganache from "../assets/ganache.png";
-import robots from "../assets/robots.png";
-import polygon from "../assets/polygon.png";
-import meter from "../assets/meter.png";
-
-import about01 from "../assets/about01.png";
-import about02 from "../assets/about02.png";
-import about03 from "../assets/about03.png";
-import about04 from "../assets/about04.png";
-import about05 from "../assets/about05.png";
-import about06 from "../assets/about06.png";
-import about07 from "../assets/about07.png";
-import about08 from "../assets/about08.png";
-import about09 from "../assets/about09.png";
-import about10 from "../assets/about10.png";
-import about11 from "../assets/about11.png";
-import about12 from "../assets/about12.png";
-import about13 from "../assets/about13.png";
-import about14 from "../assets/about14.png";
-import about15 from "../assets/about15.png";
-
 import profile from "../assets/profile.png";
 import circle from "../assets/circle.svg";
 import logo from "../assets/logo.png";
 
 export default {
   email,
-  git,
-  javascript,
   solidity,
-  node,
   python,
   react,
-  redux,
-  typescript,
-  about01,
-  about02,
-  about03,
-  about04,
-  about05,
-  about06,
-  about07,
-  about08,
-  about09,
-  about10,
-  about11,
-  about12,
-  about13,
-  about14,
-  about15,
   profile,
   circle,
   logo,
-  whatsapp,
-  ganache,
-  robots,
-  polygon,
-  meter,
 };
