c10e147806921567bc64c3d35a2bd9672e89ae1e
chrisstef <xristostefanis@gmail.com>
2025-11-30 22:53:48 +0200

    updated images to webp, remove old images, format & small changes

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,5 +1,12 @@
-import React from "react";
-import { About, Footer, Header, Skills, Testimonial, Work, Copyright } from "./container";
+import {
+  About,
+  Footer,
+  Header,
+  Skills,
+  Testimonial,
+  Work,
+  Copyright,
+} from "./container";
 import { Navbar } from "./components";
 import "./App.scss";
 
--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -1,19 +1,19 @@
-import email from "../assets/email.png";
-import solidity from "../assets/solidity.png";
-import python from "../assets/python.png";
-import react from "../assets/react.png";
-import profile from "../assets/profile.png";
+import email from "../assets/email.webp";
+import solidity from "../assets/solidity.webp";
+import python from "../assets/python.webp";
+import react from "../assets/react.webp";
+import profile from "../assets/profile.webp";
 import circle from "../assets/circle.svg";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo.webp";
 import next from "../assets/next.svg";
 
 export default {
-    email,
-    solidity,
-    python,
-    react,
-    profile,
-    circle,
-    logo,
-    next,
+  email,
+  solidity,
+  python,
+  react,
+  profile,
+  circle,
+  logo,
+  next,
 };
